92ad4a007ea4392e9fc4e978bb3e3bcfd54ad669
PIATKO01 <piatko01@kits.kfplc.com>
2022-05-10 21:42:22 +0300

    task 1

---
+++ Homework_1/task1/index.js
@@ -0,0 +1,10 @@
+const { stdin, stdout } = require('process')
+
+function reverseStr(data) {
+  return data.toString().split('').reverse().join('').trim()
+}
+stdin.on('data', (data) => {
+  const newStr = reverseStr(data)
+
+  stdout.write(`${newStr} \n`)
+})
